2c4bfd4d3b15151c71b558252ee2f6cfcd6343ba
InonBaluka21 <inonba@post.bgu.ac.il>
2026-05-03 13:15:25 +0300

    Added permissions that a manager can have

---
+++ src/main/java/com/ticketing/domain/user/ManagerPermission.java
@@ -0,0 +1,11 @@
+package com.ticketing.domain.user;
+
+public enum ManagerPermission {
+    MAP_DEFINITION,
+    INVENTORY_MGMT,
+    POLICY_MODIFICATION,
+    PERSONNEL_MGMT,
+    VIEW_REPORTS,
+    EVENT_LIFECYCLE,
+    HANDLE_INQUIRIES
+}
