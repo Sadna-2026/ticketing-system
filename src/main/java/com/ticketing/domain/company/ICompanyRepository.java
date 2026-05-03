49f84a75a36355a4f81e36080daac6859ed24e4e
InonBaluka21 <inonba@post.bgu.ac.il>
2026-05-03 22:07:57 +0300

    Added useful methods to the company repo interface for tests

--- src/main/java/com/ticketing/domain/company/ICompanyRepository.java
+++ src/main/java/com/ticketing/domain/company/ICompanyRepository.java
@@ -2,5 +2,6 @@ package com.ticketing.domain.company;
 import com.ticketing.infrastructure.IRepository;
 
 public interface ICompanyRepository extends IRepository<Company> {
-    
+    Company findByName(String name);
+    boolean existsByName(String name);
 }
