912ef29b2e75bbf029b7a42febdb7641d0b130ff
shpigelgi <gilad.shpigelman@gmail.com>
2026-05-26 16:38:39 +0300

    Remove unused import statement from CompanyView.java

--- src/main/java/com/ticketing/presentation/vaadin/views/CompanyView.java
+++ src/main/java/com/ticketing/presentation/vaadin/views/CompanyView.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 import com.ticketing.application.dto.CompanyPublicDTO;
