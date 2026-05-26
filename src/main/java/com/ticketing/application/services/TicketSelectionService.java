79bc4072dfc51e4a5cf1e0ff4507cb5775d7bc45
InonBaluka21 <inonba@post.bgu.ac.il>
2026-05-26 20:30:14 +0300

    Refactoring order related services to use domain service

--- src/main/java/com/ticketing/application/services/TicketSelectionService.java
+++ src/main/java/com/ticketing/application/services/TicketSelectionService.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ticketing.application.SelectionRequest;
-import com.ticketing.application.SelectionRequest.GAPick;
-import com.ticketing.application.SelectionRequest.SeatPick;
 import com.ticketing.domain.event.Event;
 import com.ticketing.domain.event.EventStatus;
 import com.ticketing.domain.event.IEventRepository;
